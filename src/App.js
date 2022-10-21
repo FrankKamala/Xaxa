5b7c160a54bab0ee3d0494df6273d723faecd75e
FrankKamala <frankkamala@gmail.com>
2022-10-21 18:39:47 +0300

    icons import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from '././components/header/Header';
-import DateCard from   './components/datingcards/Datecard'
+import DateCard from   './components/datingcards/Datecard';
+import SwipeButton from'./components/swipebuttons/SwipeButton';
 
 function App() {
   return (
     <div className="app">
       <Header  />
      < DateCard />
+<SwipeButton/>
     </div>
   );
 }
---
+++ src/components/swipebuttons/SwipeButton.js
@@ -0,0 +1,9 @@
+import React from ' react';
+import './SwipeButton.css'
+import ReplayIcon from '@mui/icons-material/Replay'
+import CloseIcon from '@mui/icons-material/Close'
+import StarRateIcon from '@mui/icons-material/StarRate'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import FlashOnIcon from '@mui/icons-material/FlashOn'
+import IconButton from '@mui/icons-material/IconButton'
+
