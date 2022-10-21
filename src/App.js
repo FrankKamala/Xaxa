df30eddebdb81cec6fee68b13c4fb514569ad1a0
FrankKamala <frankkamala@gmail.com>
2022-10-21 12:39:07 +0300

    add dte card components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from '././components/header/Header';
+import DateCard from   './components/datingcards/Datecard'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <DateCard></DateCard>
     </div>
   );
 }
